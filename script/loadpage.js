63dd4dc6ec546db490bc0c04921f424e2f9b16fa
UIT_20521888 <20521888@gm.uit.edu.vn>
2023-02-07 08:21:06 +0700

    finished add comment and update footer

--- script/loadpage.js
+++ script/loadpage.js
@@ -1,49 +1,49 @@
-var o = 1 
-function tim(){
-  var timkiem = document.getElementById("tim");
-  if(o==1){
-    timkiem.style.visibility="visible";
-    o=0;
-  } 
-  else{
-    timkiem.style.visibility="hidden";
-    o=1;
-  }
-}
+// var o = 1 
+// function tim(){
+//   var timkiem = document.getElementById("tim");
+//   if(o==1){
+//     timkiem.style.visibility="visible";
+//     o=0;
+//   } 
+//   else{
+//     timkiem.style.visibility="hidden";
+//     o=1;
+//   }
+// }
 
-function search(){
-  const searchButton = document.getElementById('search-button');
-  searchButton.addEventListener('click', function() {
-  const searchInput = document.getElementById('search-input').value;
-  window.location.href = `images.html?search=${searchInput}`;
-  });
-}
+// function search(){
+//   const searchButton = document.getElementById('search-button');
+//   searchButton.addEventListener('click', function() {
+//   const searchInput = document.getElementById('search-input').value;
+//   window.location.href = `images.html?search=${searchInput}`;
+//   });
+// }
 
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+// // function showSlides(n) {
+// //   let i;
+// //   let slides = document.getElementsByClassName("mySlides");
+// //   let dots = document.getElementsByClassName("dot");
+// //   if (n > slides.length) {slideIndex = 1}
+// //   if (n < 1) {slideIndex = slides.length}
+// //   for (i = 0; i < slides.length; i++) {
+// //     slides[i].style.display = "none";
+// //   }
+// //   for (i = 0; i < dots.length; i++) {
+// //     dots[i].className = dots[i].className.replace(" active", "");
+// //   }
+// //   slides[slideIndex-1].style.display = "block";
+// //   dots[slideIndex-1].className += " active";
+// // }
